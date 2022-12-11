67f911f9bc4d73e24d6bef2025f98ebab3c0e5d2
sadmanador <107940313+GaziTasnuvaFarhana@users.noreply.github.com>
2022-12-11 19:43:22 +0600

    module 58-5 done!

--- src/components/Home/Registration.js
+++ src/components/Home/Registration.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 
 const Registration = () => {
+
+    const { createUser } = useContext(AuthContext)
     const handleSubmit = event => {
         event.preventDefault();
         const form = event.target;
@@ -9,6 +12,16 @@ const Registration = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(username, email, password);
+
+        createUser(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log('Registered user', user)
+        })
+        .catch(error => {
+            console.error(error);
+        })
+
     }
 
     return (
@@ -16,11 +29,11 @@ const Registration = () => {
             <h2>Please fill up the registration form</h2>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username: </label>
-                <input type="text" placeholder='Your username' name='username' required/><br />
+                <input type="text" placeholder='Your username' name='username' required /><br />
                 <label htmlFor="email">Email: </label>
-                <input type="email" placeholder='Your Email' name='email' required/><br />
+                <input type="email" placeholder='Your Email' name='email' required /><br />
                 <label htmlFor="password">Password: </label>
-                <input type="password"  placeholder='Your password' name='password' required/><br />
+                <input type="password" placeholder='Your password' name='password' required /><br />
                 <button type='submit'>Register</button>
             </form>
             <Link to='/login'>Already have an account?</Link>
